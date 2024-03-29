69f8df44c17dd333b1751a0eaa9138044b1ca4f7
Dima <skydmytro@gmail.com>
2024-03-29 16:06:07 +0100

    First commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./header.style.scss"
+
+export const Header = () => {
+  return (
+    <div className='header'>
+        <h1 className='title'>
+            Shiro's typing test
+        </h1>
+        <p className='main-text'>
+            open-source typing test
+        </p>
+    </div>
+  )
+}
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "../../components/Footer/Footer"
+import { Header } from "../../components/Header/Header"
+import { TypingTest } from "../../components/TypingTest/TypingTest"
+
+export const MainPage = () => {
+  return (
+    <div>
+        <Header/>
+        <TypingTest/>
+        <Footer />
+    </div>
+  )
+}
