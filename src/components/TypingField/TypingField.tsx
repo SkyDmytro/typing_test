cfb0a799302ec240837f55b1d87853229f83829b
Dima <skydmytro@gmail.com>
2024-04-02 15:39:09 +0200

    new version of typing field

--- src/components/TypingField/TypingField.tsx
+++ src/components/TypingField/TypingField.tsx
@@ -1,27 +1,40 @@
-import { TypingFieldWord } from './components/TypingFieldWord';
-import "./typingField.style.scss"
-import classNames from 'classnames';
+import { ReactNode, useState } from "react";
+import "./typingField.style.scss";
 
-interface TypingFieldProps{
-  string:string,
-  activeWordAndSymbol:number[]
-}
+export const TypingField = () => {
+  const [inputText, setInputText] = useState("");
+  const text =
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur assumenda ex suscipit corrupti error ullam itaque deserunt. Recusandae earum maiores cumque id, itaque ea quae molestias temporibus error libero.";
 
-export const TypingField = ({string}:TypingFieldProps) => {
-  const newString = string;
-  const words = newString.split(" ")
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
 
+  const getStyledWords = (): ReactNode => {
+    const inputArray = inputText.split("");
+    const otherText = text.slice(inputText.length, text.length);
+    return (
+      <>
+        {inputArray.map((character, idx) => {
+          if (character === text.charAt(idx)) {
+            return <span className="correct">{text.charAt(idx)}</span>;
+          } else {
+            return <span className="incorrect">{text.charAt(idx)}</span>;
+          }
+        })}
+        <span className="caret-right"></span>
+        {otherText}
+      </>
+    );
+  };
 
   return (
-    <div className={classNames("words")}>
-    {
-      words.map((word,idx)=>{
+    <>
 
-        return (
-          <TypingFieldWord word={word} isActive={false} key={idx} activeLetter={1} idx={idx} />
-        )
-      })
-    }
-    </div>
-  )
-}
+      <div className="field">
+        <div className="words-container">{getStyledWords()}</div>
+        <input className="input" onChange={handleInputChange} autoFocus />
+      </div>
+    </>
+  );
+};
--- src/components/TypingTest/TypingTest.tsx
+++ src/components/TypingTest/TypingTest.tsx
@@ -1,53 +1,12 @@
-import { useEffect, useRef, useState } from "react";
 import { TypingField } from "../TypingField/TypingField";
-import { TypingInput } from "../TypingInput/TypingInput";
-import "./typingTest.style.scss";
-import {useToggleClassName } from "../../hooks/useAddClassName";
 
 export const TypingTest = () => {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const { add, remove } = useToggleClassName();
-  const [currentCharIndex,setCurrentCharIndex]= useState(0)
-  const [currentWordIndex, setCurrentWordIndex]=useState(0)
 
-  const text =
-    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut pariatur assumenda ex suscipit corrupti error ullam itaque deserunt. Recusandae earum maiores cumque id, itaque ea quae molestias temporibus error libero.";
-  useEffect(() => {
-    add({ classname: "caret-left", elementClassname: "00" });
-    inputRef.current?.focus()
-    return () => {
-      remove({ classname: "caret-left", elementClassname: "00" });
-    };
-  }, []);
-
-  const handleInputChange = (input: string) => {
-    const typedChar = input[input.length-1];
-    const expectedChar = text[input.length - 1];
-    console.log(typedChar, expectedChar);
-    if (typedChar === expectedChar) {
-      if(typedChar===" "){
-        setCurrentWordIndex(currentWordIndex+1)
-        setCurrentCharIndex(0)
-      }
-      else{
-        setCurrentCharIndex(currentCharIndex+1)
-      }
-      remove({classname:"caret-left",elementClassname:`${currentWordIndex}${currentCharIndex}`})
-      add({classname:"caret-left",elementClassname:`${currentWordIndex}${currentCharIndex+1}`})
-      remove(({classname:"incorrect",elementClassname:`${currentWordIndex}${currentCharIndex}`}))
-      add({classname:"correct",elementClassname:`${currentWordIndex}${currentCharIndex}`})
-    }else{
-      add(({classname:"incorrect",elementClassname:`${currentWordIndex}${currentCharIndex}`}))
-    }
-
-  };
 
   return (
     <>
-      <div className="field">
-        <TypingField string={text} activeWordAndSymbol={[1, 1]} />
-        <TypingInput onChange={handleInputChange} inputRef={inputRef} />
-      </div>
+      <TypingField/>
+      
     </>
   );
 };
