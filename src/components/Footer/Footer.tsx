1cdd48f7358d2653d4eb7d46f99d5025e5edc9f7
Dima <skydmytro@gmail.com>
2024-11-03 12:55:24 +0100

    refactor: small changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,34 +3,38 @@ import gitHubLogo from "../../assets/icons8-github-24.png";
 import "./footer.style.scss";
 import { ThemeContext } from "../../pages/MainPage/MainPage";
 import styled from "styled-components";
-
-export const Footer = () => {
-  const { theme } = useContext(ThemeContext);
-  const StyledFooter = styled.div`
-    position: fixed;
+const StyledFooter = styled.div<{ baseColor: string; activeColor: string }>`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  bottom: 0;
+  left: 0;
+  color: ${({ baseColor }) => baseColor};
+  width: 100%;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  .github {
     display: flex;
     justify-content: center;
-    bottom: 0;
-    left: 0;
-    color: ${theme.baseColor};
-    width: 100%;
-    text-align: center;
-    flex-direction: column;
-    justify-content: center;
-    .github {
-      display: flex;
-      justify-content: center;
-      gap: 10px;
+    gap: 10px;
+  }
+  .idea {
+    .highlighted {
+      color: ${({ activeColor }) => activeColor};
+      text-decoration: none;
     }
-    .idea {
-      .highlighted {
-        color: ${theme.activeColor};
-        text-decoration: none;
-      }
-    }
-  `;
+  }
+`;
+export const Footer = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <StyledFooter className="footer">
+    <StyledFooter
+      className="footer"
+      baseColor={theme.baseColor}
+      activeColor={theme.activeColor}
+    >
       <div className="github">
         Projects repository
         <a href="https://github.com/SkyDmytro/typing_test">
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,16 +3,17 @@ import "./header.style.scss";
 import { ThemeContext } from "../../pages/MainPage/MainPage";
 import styled from "styled-components";
 
+const StyledHeader1 = styled.h1<{ headerColor: string }>`
+  color: ${({ headerColor }) => headerColor};
+`;
 export const Header = () => {
   const { theme } = useContext(ThemeContext);
-  const StyledHeader1 = styled.h1`
-    color: ${theme.headerColor};
-  `;
 
   return (
     <div className="header">
-      <StyledHeader1 className="title">TYPING TEST</StyledHeader1>
-      {/* <h3 className="main-text">Typing game</h3> */}
+      <StyledHeader1 headerColor={theme.headerColor} className="title">
+        TYPING TEST
+      </StyledHeader1>
     </div>
   );
 };
